047d8ab6a55094bb509aa24439638dc18f8f08f3
nmaravante <nmaravante2@gmail.com>
2020-03-29 20:29:41 +0530

    new commit

--- user_activity/settings.py
+++ user_activity/settings.py
@@ -126,3 +126,11 @@ STATIC_URL = '/static/'
 #     os.path.join(BASE_DIR, "static"),
 
 # ]
+
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
